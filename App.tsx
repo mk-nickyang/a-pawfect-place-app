c63184237af37bd827e311f83494fba3395ff83b
nickY <nickyang0501+work@gmail.com>
2024-03-18 13:09:37 +1000

    add mmkv

--- App.tsx
+++ App.tsx
@@ -1,18 +1,32 @@
 import * as Sentry from '@sentry/react-native';
 import { ThemeProvider } from '@shopify/restyle';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
 import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from 'react-native-safe-area-context';
 
 import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { clientPersister } from '@/modules/storage';
 import { AppNavigator } from '@/navigation/AppNavigator';
 import theme from '@/theme';
 
-Sentry.init({ dsn: process.env.EXPO_PUBLIC_SENTRY_DSN });
+Sentry.init({ dsn: process.env.EXPO_PUBLIC_SENTRY_DSN, enabled: !__DEV__ });
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24 hours
+    },
+  },
+});
+
+persistQueryClient({
+  // @ts-expect-error Ignore queryClient type mismatch
+  queryClient,
+  persister: clientPersister,
+});
 
 function App() {
   return (
---
+++ src/modules/storage.ts
@@ -0,0 +1,21 @@
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { MMKV } from 'react-native-mmkv';
+
+const storage = new MMKV();
+
+const PersistedStorage = {
+  setItem: (key: string, value: Parameters<typeof storage.set>[1]) => {
+    storage.set(key, value);
+  },
+  getItem: (key: string) => {
+    const value = storage.getString(key);
+    return value === undefined ? null : value;
+  },
+  removeItem: (key: string) => {
+    storage.delete(key);
+  },
+};
+
+export const clientPersister = createSyncStoragePersister({
+  storage: PersistedStorage,
+});
